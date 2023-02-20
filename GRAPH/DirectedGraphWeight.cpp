be232f139bdd7380d2629b9f49374ea9e2593615
Karishma Chaudhary <113928981+chaudhary-99@users.noreply.github.com>
2023-02-20 12:51:52 +0530

    Add files via upload

---
+++ GRAPH/DirectedGraphWeight.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+using namespace std;              
+int main()
+{
+    int v, e;
+    cout << "No. of vetices ";
+    cin >> v;
+    cout << "No. of edges ";
+    cin >> e;
+    vector<pair<int,int>> Adjalist[v];
+    int Adjacency_Matrix[v][v];
+    int Incidency_Matrix[e][v];
+    for(int i=0;i<v;i++){
+        for(int j=0;j<v;j++){
+            Adjacency_Matrix[i][j]=0;
+        }
+    }
+    for(int i=0;i<e;i++){
+        for(int j=0;j<v;j++){
+            Incidency_Matrix[i][j]=0;
+        }
+    }
+
+    for (int i = 0; i < e; i++)
+    {
+        cout<<"Edge and weight "<<i+1<<":- ";
+        int a,b,w;
+        cin >> a >> b>>w;
+        Adjalist[a].push_back(make_pair(b,w));
+        Adjacency_Matrix[a][b]=1;
+        Incidency_Matrix[a][i]=1;
+    }
+    cout<<"ADJACENCY LINKED LIST\n";
+    for (int i = 0; i <v; i++)
+    {   
+        cout << i << ": ";
+        for (int j = 0; j < Adjalist[i].size(); j++)
+        {
+            cout<< Adjalist[i][j].first<<" "<<Adjalist[i][j].second;
+        }
+        cout << endl;
+    }
+    cout<<"ADJACENCY MATRIX\n";
+    for (int i = 0; i <v; i++)
+    {   
+        cout << i << ": ";
+        for (int j = 0; j < v; j++)
+        {
+            cout<< Adjacency_Matrix[i][j]<<" ";
+        }
+        cout << endl;
+    }
+    cout<<"INCIDENCE MATRIX\n";
+    for (int i = 0; i <v; i++)
+    {   
+        cout << i << ": ";
+        for (int j = 0; j < e; j++)
+        {
+            cout<< Incidency_Matrix[i][j]<<" ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
